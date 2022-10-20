77b2ce673a66653e419eaca6fa86c3ec7560da1e
srikresna <srikresna.dewa@gmail.com>
2022-10-20 11:07:41 +0700

    newww

---
+++ factorialDoWhile.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class factorialDoWhile{
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        int number, factorial, i;
+
+        System.out.print("Enter a number : ");
+        number = input.nextInt();
+
+        factorial = 1;
+        i = 1;
+        do {
+            factorial = factorial * i;
+            i++;
+
+        } while (i <= number);
+
+        System.out.printf("The factorial of %d is %d", number, factorial);
+    }
+}
