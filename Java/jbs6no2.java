d8b1ec5e0b644e73c9adf75a06fd1a9c9e3ba354
srikresna <srikresna.dewa@gmail.com>
2022-10-23 18:37:29 +0700

    add indent

--- Java/jbs6no2.java
+++ Java/jbs6no2.java
@@ -12,13 +12,13 @@ public class jbs6no2 {
 
         System.out.print("Enter a number range: ");
         n = input.nextInt();
-        System.out.println("The number of even numbers from 1 to " + n + " is " + n/2 );
-        for(i = 1; i <= n; i++){
-            if(i % 2 != 0)
-            continue; 
+        System.out.println("The number of even numbers from 1 to " + n + " is " + n / 2);
+        for (i = 1; i <= n; i++) {
+            if (i % 2 != 0)
+                continue;
             count++;
             System.out.println(String.format("Even number %d is %d", count, i));
-            sum += i;  
+            sum += i;
         }
         avarage = (float) sum / count;
 
