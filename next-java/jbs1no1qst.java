3b050b56ca1c15bc14300a7fd8a157b3061e41fc
srikresna <srikresna383@gmail.com>
2023-02-16 19:21:03 +0700

    finish jbs1no1

---
+++ next-java/jbs1no1qst.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class jbs1no1qst {
+    static Scanner sc = new Scanner(System.in);
+    static int taskScore, midScore, finalScore;
+    static 
+    public static void main(String[] args) {
+
+        System.out.println("Program to Calculate Final Grade");
+        System.out.println("====================");
+        System.out.println("Input Task Score : ");
+        taskScore = sc.nextInt();
+        System.out.println("Input Mid Score : ");
+        midScore = sc.nextInt();
+        System.out.println("Input Final Score : ");
+        finalScore = sc.nextInt();
+        System.out.println("====================");
+        System.out.println("====================");
+        System.out.printf("");
+    }
+}
+    
