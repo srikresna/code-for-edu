875a43d8a28e46c4b6293e62fa457fa8988ed1ae
srikresna <srikresna383@gmail.com>
2023-02-16 19:23:53 +0700

    finish questions

---
+++ next-java/jbs1no3qst.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class jbs1no3qst {
+    static Scanner input = new Scanner(System.in);
+    static String flowersType[] = {"Aglaonema", "Taro", "Alocasia", "Rose"};
+    static String branches[] = {"RoyalGarden1", "RoyalGarden2", "RoyalGarden3", "RoyalGarden4"};
+    static int prices[] = {75_000, 50_000, 60_000, 10_000};
+    static int amount[][] = {
+            {9, 3, 15, 2}, //before is 10, 5, 15, 7
+            {6, 11, 9, 12},
+            {2, 10, 10, 5},
+            {5, 7, 12, 9}
+    };
+
+    public static void main(String[] args) {
+        for (int row = 0; row < amount.length; row++) {
+            int stock = 0;
+            for (int col = 0; col < amount[0].length; col++) {
+                stock+=amount[row][col];
+            }
+            System.out.println("Stock of Branch " + row + " = " + stock);
+        }
+
+        System.out.println("====================");
+        System.out.println("Stock has decreased since the flowers are wither dead or dead");
+        int income = 0;
+        for (int i = 0; i < amount[0].length; i++) {
+            income += amount[0][i] * prices[i];
+        }
+        System.out.println("Income of Royal Garden 1 if all flowers are sold out = " + income);
+
+    }
+}
---
+++ next-java/jbs1no4qst.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class jbs1no4qst {
+    static Scanner input = new Scanner(System.in);
+    //function to display fibonacci row using loop
+    static void fibonacciLoop(int n){
+        int a = 0, b = 1, c;
+        System.out.print(a + " " + b + " ");
+        for (int i = 2; i < n; i++) {
+            c = a + b;
+            System.out.print(c + " ");
+            a = b;
+            b = c;
+        }
+    }
+    //function to display fibonacci row using recursion
+static int fibonacciRecursion(int n){
+        if (n == 0 || n == 1) {
+            return n;
+        } else {
+            return fibonacciRecursion(n - 1) + fibonacciRecursion(n - 2);
+        }
+    }
+    public static void main(String[] args) {
+        //fibonnaci row: 0, 1, 1, 2, 3, 5, 8, 13, 21
+        System.out.print("Enter the number of fibonacci row: ");
+        int n = input.nextInt();
+        System.out.println("====================");
+        System.out.println("Fibonacci row using loop");
+        fibonacciLoop(n);
+        System.out.println("\n====================");
+        System.out.println("Fibonacci row using recursion");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacciRecursion(i) + " ");
+        }
+
+    }
+}
