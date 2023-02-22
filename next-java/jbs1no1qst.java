5c576fd152d29465872988b231204cb38ad5afa5
Sri Kresna <srikresna383@gmail.com>
2023-02-22 20:54:28 +0700

    fix: missing code

--- next-java/jbs1no1qst.java
+++ next-java/jbs1no1qst.java
@@ -4,20 +4,55 @@ import java.util.Scanner;
 public class jbs1no1qst {
     static Scanner sc = new Scanner(System.in);
     static int taskScore, midScore, finalScore;
-    static 
+    static double calculateFinalGrade() {
+        double finalGrade = 0;
+        finalGrade = (taskScore * 0.2) + (midScore * 0.35) + (finalScore * 0.45);
+        return finalGrade;
+    }
+    static String getGrade() {
+        String grade = "";
+        if (calculateFinalGrade() > 80 && calculateFinalGrade() <= 100) {
+            grade = "A";
+        } else if (calculateFinalGrade() > 73 && calculateFinalGrade() <= 80) {
+            grade = "B+";
+        } else if (calculateFinalGrade() > 65 && calculateFinalGrade() <= 73) {
+            grade = "B";
+        } else if (calculateFinalGrade() > 60 && calculateFinalGrade() <= 65) {
+            grade = "C+";
+        } else if (calculateFinalGrade() > 50 && calculateFinalGrade() <= 60) {
+            grade = "C";
+        } else if (calculateFinalGrade() > 39 && calculateFinalGrade() <= 50) {
+            grade = "D";
+        } else {
+            grade = "E";
+        }
+        return grade;
+    }
+    static String getPassed() {
+        String passed = "";
+        if (getGrade().equals("A") || getGrade().equals("B+") || getGrade().equals("B") || getGrade().equals("C+") || getGrade().equals("C")) {
+            passed = "Passed";
+        } else {
+            passed = "Failed";
+        }
+        return passed;
+    }
     public static void main(String[] args) {
 
         System.out.println("Program to Calculate Final Grade");
         System.out.println("====================");
-        System.out.println("Input Task Score : ");
+        System.out.print("Input Task Score : ");
         taskScore = sc.nextInt();
-        System.out.println("Input Mid Score : ");
+        System.out.print("Input Mid Score : ");
         midScore = sc.nextInt();
-        System.out.println("Input Final Score : ");
+        System.out.print("Input Final Score : ");
         finalScore = sc.nextInt();
         System.out.println("====================");
         System.out.println("====================");
-        System.out.printf("");
+        System.out.println("Final Grade : " + calculateFinalGrade());
+        System.out.println("Grade : " + getGrade());
+        System.out.println("====================");
+        System.out.println(getPassed());
     }
 }
     
