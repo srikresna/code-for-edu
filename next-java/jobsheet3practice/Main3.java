96bbd850fa6b1a15b6a8101d6bf4fc115a975524
srikresna <srikresna383@gmail.com>
2023-03-04 21:26:11 +0700

    finish task jbs3

--- next-java/jobsheet3practice/Main3.java
+++ next-java/jobsheet3practice/Main3.java
@@ -4,9 +4,31 @@ import java.util.Scanner;
 public class Main3 {
     public static void main(String[] args) {
         Student[] studentsArray = new Student[3];
+        Scanner sc = new Scanner(System.in);
 
         for(int i = 0; i < studentsArray.length; i++) {
-            System.out.print("Insert " + i + " student data");
+            studentsArray[i] = new Student();
+
+            System.out.println("Insert " + (i + 1) + " student data");
+            System.out.print("insert name : ");
+            studentsArray[i].name = sc.nextLine();
+            System.out.print("Insert nim : ");
+            studentsArray[i].nim = sc.nextLine();
+            System.out.print("Insert gender : ");
+            studentsArray[i].gender = sc.nextLine();
+            System.out.print("Insert ipk : ");
+            studentsArray[i].ipk = sc.nextDouble();
+            sc.nextLine();
         }
+
+        for (int i = 0; i < studentsArray.length; i++) {
+            System.out.println((i + 1) + " Student Data");
+            System.out.println("Name : " + studentsArray[i].name);
+            System.out.println("Nim : " + studentsArray[i].nim);
+            System.out.println("Gender : " + studentsArray[i].gender);
+            System.out.println("Ipk score : " + studentsArray[i].ipk);
+        }
+
+        System.out.println("Average ipk of all students : " + studentsArray[0].getAvergaeIpk(studentsArray));
     }
 }
--- next-java/jobsheet3practice/Student.java
+++ next-java/jobsheet3practice/Student.java
@@ -1,5 +1,16 @@
 package jobsheet3practice;
 
 public class Student {
-    public String name, nim, gender, ipk;
+    public String name, nim, gender;
+    public Double ipk;
+    public Student() {
+    }
+
+    public Double getAvergaeIpk(Student[] studentsArray) {
+        Double averageIpk = 0.0;
+        for(int i = 0; i < studentsArray.length; i++) {
+            averageIpk += studentsArray[i].ipk;
+        }
+        return averageIpk / studentsArray.length;
+    }
 }
