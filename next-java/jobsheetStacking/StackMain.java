4f137d4dff5404e29f937f32282fc9c9671cf66a
srikresna <srikresna383@gmail.com>
2023-04-07 20:38:46 +0700

    1st lab question

--- next-java/jobsheetStacking/StackMain.java
+++ next-java/jobsheetStacking/StackMain.java
@@ -4,14 +4,17 @@ public class StackMain {
     public static void main(String[] args) {
         Stack stk = new Stack(5);
         stk.push(15);
-        stk.push(30);
-        stk.push(45);
-        stk.push(60);
-        stk.push(75);
+        stk.push(27);
+        stk.push(13);
         stk.print();
+        stk.push(11);
         stk.push(34);
         stk.pop();
         stk.peek();
         stk.print();
+        stk.push(18);
+        stk.push(40);
+        stk.print();
+        // 40 will not be pushed because the stack is full
     }
 }
