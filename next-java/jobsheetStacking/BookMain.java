1ab5f6265e30a72a3c4927ed08dd111844450aa5
srikresna <srikresna383@gmail.com>
2023-04-07 21:05:04 +0700

    2nd lab activities

--- next-java/jobsheetStacking/BookMain.java
+++ next-java/jobsheetStacking/BookMain.java
@@ -1,4 +1,5 @@
 package jobsheetStacking;
+
 import java.util.Scanner;
 
 public class BookMain {
@@ -8,30 +9,50 @@ public class BookMain {
 
         char choose;
         do {
-            System.out.print("Title : ");
-            String title = sc.nextLine();
+            System.out.println("Select operation:");
+            System.out.println("1. Push");
+            System.out.println("2. Pop");
+            System.out.println("3. Peek");
+            System.out.println("4. Print");
+            int option = sc.nextInt();
+            sc.nextLine();
 
-            System.out.print("Author name : ");
-            String authorName = sc.nextLine();
+            switch (option) {
+                case 1:
+                    System.out.print("Title : ");
+                    String title = sc.nextLine();
 
-            System.out.print("Published year : ");
-            int publishedYear = sc.nextInt();
+                    System.out.print("Author name : ");
+                    String authorName = sc.nextLine();
 
-            System.out.print("Pages amount : ");
-            int pagesAmount = sc.nextInt();
+                    System.out.print("Published year : ");
+                    int publishedYear = sc.nextInt();
 
-            System.out.print("Price : ");
-            int price = sc.nextInt();
+                    System.out.print("Pages amount : ");
+                    int pagesAmount = sc.nextInt();
 
-            Book bk = new Book(title, authorName, publishedYear, pagesAmount, price);
-            System.out.print("Do you want to add new data to stack (y/n) ? ");
+                    System.out.print("Price : ");
+                    int price = sc.nextInt();
+
+                    Book bk = new Book(title, authorName, publishedYear, pagesAmount, price);
+                    st.push(bk);
+                    break;
+                case 2:
+                    st.pop();
+                    break;
+                case 3:
+                    st.peek();
+                    break;
+                case 4:
+                    st.print();
+                    break;
+                default:
+                    System.out.println("Invalid option!");
+            }
+
+            System.out.print("Do you want to continue (y/n) ? ");
             choose = sc.next().charAt(0);
             sc.nextLine();
-            st.push(bk);
         } while (choose == 'y' || choose == 'Y');
-        st.print();
-        st.pop();
-        st.peek();
-        st.print();
     }
 }
