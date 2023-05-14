1b617ea4e0cabc726053748f1dda59a49bf1f53d
srikresna <srikresna383@gmail.com>
2023-05-14 18:40:01 +0700

    add 2nd lab activity

--- next-java/jobsheetLinkedList/SLLMain.java
+++ next-java/jobsheetLinkedList/SLLMain.java
@@ -14,5 +14,16 @@ public class SLLMain {
         sll.print();
         sll.insertAt(3, 833);
         sll.print();
+        //2nd lab activity
+        System.out.println("Data in 1st index : " + sll.getData(1));
+        System.out.println("Data 3 is in index : " + sll.indexOf(760));
+        sll.remove(999);
+        sll.print();
+        sll.removeAt(0);
+        sll.print();
+        sll.removeFirst();
+        sll.print();
+        sll.removeLast();
+        sll.print();
     }
 }
--- next-java/jobsheetLinkedList/SingleLinkedList.java
+++ next-java/jobsheetLinkedList/SingleLinkedList.java
@@ -74,4 +74,89 @@ public class SingleLinkedList {
             if (temp.next.next == null) tail = temp.next;
         }   
     }
+
+    //start of 2nd lab activity
+    public int getData(int index) {
+        Node tmp = head;
+        for (int i = 0; i < index; i++) {
+            tmp = tmp.next;
+        }
+        return tmp.data;
+    }
+
+    public int indexOf(int key) {
+        Node tmp = head;
+        int index = 0;
+        while (tmp != null && tmp.data != key) {
+            tmp = tmp.next;
+            index++;
+        }
+
+        if (tmp == null) {
+            return -1;
+        } else {
+            return index;
+        }
+    }
+
+    public void removeFirst() {
+        if (isEmpty()) {
+            System.out.println("Linked list is empty, cannot remove a data");
+        } else if (head == tail) {
+            head = tail = null;
+        } else {
+            head = head.next;
+        }
+    }
+
+    public void removeLast() {
+        if (isEmpty()) {
+            System.out.println("Linked list is empty, cannot remove a data");
+        } else if (head == tail) {
+            head = tail = null;
+        } else {
+            Node temp = head;
+            while (temp.next != tail) {
+                temp = temp.next;
+            }
+            temp.next = null;
+            tail = temp;
+        }
+    }
+
+    public void remove (int key) {
+        if (isEmpty()) {
+            System.out.println("Linked list is empty, cannot remove a data");
+        } else {
+            Node temp = head;
+            while (temp != null) {
+                if ((temp.data == key) && (temp == head)) {
+                    this.removeFirst();
+                    break;
+                } else if (temp.next.data == key) {
+                    temp.next = temp.next.next;
+                    if (temp.next == null) {
+                        tail = temp;
+                    }
+                    break;
+                }
+                temp = temp.next;
+            }
+        }
+    }
+
+    public void removeAt(int index) {
+        if (index == 0) {
+            removeFirst();
+        } else {
+            Node temp = head;
+            for (int i = 0; i < index - 1; i++) {
+                temp = temp.next;
+            }
+            temp.next = temp.next.next;
+            if (temp.next == null) {
+                tail = temp;
+            }
+        }
+    }
 }
