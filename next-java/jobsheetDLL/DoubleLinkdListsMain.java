0ae7d95cd2da2fcb02f78f8695e7ccd8f4fa9121
srikresna <srikresna383@gmail.com>
2023-05-19 19:58:13 +0700

    add jbs ddl

---
+++ next-java/jobsheetDLL/DoubleLinkdListsMain.java
@@ -0,0 +1,25 @@
+package jobsheetDLL;
+
+import javax.xml.transform.Source;
+
+public class DoubleLinkdListsMain {
+    public static void main(String[] args) throws Exception {
+        DoubleLinkedLists dll = new DoubleLinkedLists();
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("==============================");
+        dll.addFirst(3);
+        dll.addLast(4);
+        dll.addFirst(7);
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("==============================");
+        dll.add(40, 1);
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("===============================");
+        dll.clear();
+        dll.print();
+        System.out.println("Size : " + dll.size());
+    }
+}
