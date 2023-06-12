9212be087ed15b8691e6f47f0c8065d01c819dfe
srikresna <srikresna383@gmail.com>
2023-06-12 20:37:46 +0700

    feat: add practicum graph

---
+++ next-java/jobsheetGraph/Graph.java
@@ -0,0 +1,74 @@
+package jobsheetGraph;
+
+public class Graph {
+    int vertex;
+    LinkedList list[];
+
+    public Graph(int vertex) {
+        this.vertex = vertex;
+        list = new LinkedList[vertex];
+        for (int i = 0; i < vertex; i++) {
+            list[i] = new LinkedList();
+        }
+    }
+
+    public void addEdge(int source, int destination) {
+        // add edge
+        list[source].addFirst(destination);
+
+        // add back edge (for undirected)
+        list[destination].addFirst(source);
+    }
+
+    public void degree(int source) throws Exception {
+        // degree undirected graph
+        System.out.println("degree vertex " + source + " : " + list[source].size());
+
+        // degree directed graph
+        // indegree
+        int k, totalIn = 0, totalOut = 0;
+        for (int i = 0; i < vertex; i++) {
+            for (int j = 0; j < list[i].size(); j++) {
+                if (list[i].get(j) == source) {
+                    ++totalIn;
+                }
+            }
+            // outdegree
+            for (k = 0; k < list[source].size(); k++) {
+                list[source].get(k);
+            }
+            totalOut = k;
+        }
+        System.out.println("Indegree dari vertex " + source + " : " + totalIn);
+        System.out.println("Outdegree dari vertex " + source + " : " + totalOut);
+        System.out.println("degree vertex " + source + " : " + (totalIn + totalOut));
+    }
+
+    public void removeEdge(int source, int destination) throws Exception {
+        for (int i = 0; i < vertex; i++) {
+            if (i == destination) {
+                list[source].remove(destination);
+            }
+        }
+    }
+
+    public void removeAllEdges() {
+        for (int i = 0; i < vertex; i++) {
+            list[i].clear();
+        }
+        System.out.println("Graph berhasil dikosongkan");
+    }
+
+    public void printGraph() throws Exception {
+        for (int i = 0; i < vertex; i++) {
+            if (list[i].size() > 0) {
+                System.out.print("Vertex " + i + " terhubung dengan: ");
+                for (int j = 0; j < list[i].size(); j++) {
+                    System.out.print(list[i].get(j) + " ");
+                }
+                System.out.println("");
+            }
+        }
+        System.out.println(" ");
+    }
+}
---
+++ next-java/jobsheetGraph/GraphMain.java
@@ -0,0 +1,18 @@
+package jobsheetGraph;
+
+public class GraphMain {
+    public static void main(String[] args) throws Exception {
+        Graph graph = new Graph(6);
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 4);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 4);
+        graph.addEdge(3, 0);
+        graph.printGraph();
+        graph.degree(2);
+
+    }
+}
